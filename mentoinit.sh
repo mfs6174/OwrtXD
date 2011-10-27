22bfaed825c54b735596b6d03c1772b7571457b1
Xijin Zhang <mfs6174@gmail.com>
2011-10-28 01:13:49 +0800

    加入安装库的代码

--- mentoinit.sh
+++ mentoinit.sh
@@ -3,6 +3,7 @@ cp conf/network
 cp mentohust/mento /usr/sbin/
 chmod +x /usr/sbin/mento
 cp -r mentohust/mentohust /etc/
+opkg install packages/lippcap.ipk
 echo " ">>mentohust/mentohust.conf
 cp mentohust/mentohust.conf /etc/
 echo " ">>start.sh
